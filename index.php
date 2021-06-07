806cb162a818979600dde64e1488bdf8fe1ab47b
vitorzf <vitor.pereira@fabricadecodigos.com.br>
2021-06-07 17:55:42 -0300

    Armaria

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site do Armaria</title>
+</head>
+<body>
+    <h1>Bem-vindo ao site do casulu</h1>
+    <hr>
+    <a href="https://thumbs.gfycat.com/DimwittedSophisticatedBarnswallow-max-1mb.gif">
+        <button>Clique aqui</button>
+    </a>
+</body>
+</html>
